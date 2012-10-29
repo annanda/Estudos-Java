b5daf8353641ecbd7472793c4728fac9572d6466
Annanda Sousa <annanda.sousa@gmail.com>
2012-10-29 09:27:15 -0200

    Alterações em scrapbook para aprender a inspecionar vários linhas de ccódigo.
    Feito o exercício de Laboratório número 1: criada a classe Carro.

---
+++ src/annanda/lab1/Carro.java
@@ -0,0 +1,16 @@
+
+public class Carro {
+	
+	public String modelo;
+	public double preco; 
+	public double consumo_medio;
+	public boolean novo; 
+	
+	public Carro(String modelo, double preco, double consumo_medio, boolean novo){
+		this.modelo = modelo;
+		this.preco = preco;
+		this.consumo_medio = consumo_medio;
+		this.novo = novo; 
+	}
+
+}
