a9aad8bc40cfddd72cec75eae9d1295ae00ef198
Annanda Sousa <annanda.sousa@gmail.com>
2012-10-29 08:45:20 -0200

    Criada a primeira classe que faz uma soma. Criado o arquivo scrapbook para inspecionar trechos de códigos presente no projeto.

---
+++ src/annanda/lab1/Ola.java
@@ -0,0 +1,12 @@
+
+public class Ola {		
+	
+	public static int soma(int a, int b){
+		return a + b;
+	}
+		
+	public static void main(String[] args){
+		System.out.println("A soma de 2 e 3 é: " + soma(2,3));
+	}
+	
+}
