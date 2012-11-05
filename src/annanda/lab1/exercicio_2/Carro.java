c0156018c84dc968b25a436b526a4738cd902f28
Annanda Sousa <annanda.sousa@gmail.com>
2012-11-05 09:45:17 -0200

    Modifiquei a classe Carro do exercicio 1, lab1. Adicionei um metodo carroVelho() para resolver a questao 2, lab 2.

--- src/annanda/lab1/exercicio_2/Carro.java
+++ src/annanda/lab1/exercicio_2/Carro.java
@@ -6,13 +6,22 @@ public class Carro {
 	public double consumo_medio;
 	public boolean novo; 
 	public Dono dono;
+	public int ano;
 	
-	public Carro(String modelo, double preco, double consumo_medio, boolean novo, Dono dono){
+	public Carro(String modelo, double preco, double consumo_medio, boolean novo, Dono dono, int ano){
 		this.modelo = modelo;
 		this.preco = preco;
 		this.consumo_medio = consumo_medio;
 		this.novo = novo;
 		this.dono = dono;
+		this.ano = ano;
+	}
+	
+	public boolean carroVelho(){
+		if (this.ano < 2002) {
+			return true;
+		}
+		return false;
 	}
 
 }
