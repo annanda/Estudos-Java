90ed21c120c62370fb4a59bab44f2e834caf80a4
Annanda Sousa <annanda.sousa@gmail.com>
2012-11-05 08:25:42 -0200

    Adicionada a pasta do exercicio 4 do Lab1. Criadas as classes Sapo e Carro.

---
+++ src/annanda/lab1/exercicio_4/Carro.java
@@ -0,0 +1,20 @@
+package exercicio_4;
+
+public class Carro {
+	public int retanguloAltura;
+	public int retanguloLargura;
+	public double velocidade;
+	public int direcao;
+	public int xPosicao;
+	public int yPosicao;
+	
+	public Carro(int retanguloAltura, int retanguloLargura, double velocidade, int direcao, int xPosicao, int yPosicao){
+		this.retanguloAltura = retanguloAltura;
+		this.retanguloLargura = retanguloLargura;
+		this.velocidade = velocidade;
+		this.direcao = direcao;
+		this.xPosicao = xPosicao;
+		this.yPosicao = yPosicao;		
+	}
+	
+}
---
+++ src/annanda/lab1/exercicio_4/Sapo.java
@@ -0,0 +1,15 @@
+package exercicio_4;
+
+public class Sapo {
+	public double raio;
+	public int vidas;
+	public int xPosicao;
+	public int yPosicao;
+	
+	public Sapo(double raio, int vidas, int xPosicao, int yPosicao){
+		this.raio = raio;
+		this.vidas = vidas;
+		this.xPosicao = xPosicao;
+		this.yPosicao = yPosicao;		
+	}
+}
