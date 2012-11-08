c459a7ac2c2299e8533e12c86513320a7d2f5842
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-11-08 20:22:54 -0200

    Carro - Posto as constantes ESQUERDA e DIREITA, para facilitar a leitura do código. Alterado um pouco a lógica do metodo "mover" para usar essas constantes e outros detalhes.

--- src/annanda/lab1/exercicio_4/Carro.java
+++ src/annanda/lab1/exercicio_4/Carro.java
@@ -8,6 +8,9 @@ public class Carro {
 	public int xPosicao;
 	public int yPosicao;
 	
+	public final int ESQUERDA=1; //usando 'final' esse atributo não pode mais ser modificado
+	public final int DIREITA=0; //usando 'final' esse atributo não pode mais ser modificado
+	
 	public Carro(int retanguloAltura, int retanguloLargura, double velocidade, int direcao, int xPosicao, int yPosicao){
 		this.retanguloAltura = retanguloAltura;
 		this.retanguloLargura = retanguloLargura;
@@ -18,12 +21,19 @@ public class Carro {
 	}
 	
 	public void mover(double dt){
-		if(this.direcao == 0){
-			this.xPosicao = this.xPosicao + (int)(this.velocidade * dt);
+
+		//a principio considera que está indo para a direita
+		int modificadorDirecao = 1;
+		
+		//se estiver indo para a esquerda, então a nova posição deve ser reduzida da original
+		if(this.direcao == ESQUERDA){
+			modificadorDirecao *= -1;
 		}
-		if(this.direcao == 1){
-			this.yPosicao = this.xPosicao - (int)(this.velocidade * dt);
-		}
+
+		//incrementa a posição x com a nova quantidade de 'passos'.
+		//aplica o modificador para tornar o valor negativo, ou manter positivo,
+		//se for para esquerda ou para direita, respectivamente
+		this.xPosicao += modificadorDirecao * (int)(this.velocidade * dt);
 		
 		if(this.xPosicao > 800){
 			this.xPosicao -=  800;
