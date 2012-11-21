65e7323fcf93717dc9a7c296e84c1cc7880e9eba
Annanda Sousa <annanda.sousa@gmail.com>
2012-11-21 20:10:16 -0200

    Desenhado e colocado em movimento todos os carros.

--- src/annanda/lab1/exercicio_4/Jogo.java
+++ src/annanda/lab1/exercicio_4/Jogo.java
@@ -21,61 +21,37 @@ public class Jogo {
 			carros[i] = new Carro();
 		}
 		
-		this.carros[0] = new Carro();
-		
-		this.carros[0].xPosicao = 200;
-		this.carros[0].yPosicao = ALTURA_DOS_CARROS + 30;
-		this.carros[0].retanguloAltura = ALTURA_DOS_CARROS;
-		this.carros[0].retanguloLargura = 100;
-		this.carros[0].velocidade = 160;
-		
-		this.carros[1].xPosicao = 400;
-		this.carros[1].yPosicao = ALTURA_DOS_CARROS + 30;
-		this.carros[1].retanguloAltura = ALTURA_DOS_CARROS;
-		this.carros[1].retanguloLargura = 100;
-		this.carros[1].velocidade = 160;
-		
+		for(int i = 0; i < 2; i++){	
+			
+			this.carros[i].xPosicao = 200 * (i+1);
+			this.carros[i].yPosicao = ALTURA_DOS_CARROS + 30;
+			this.carros[i].retanguloAltura = ALTURA_DOS_CARROS;
+			this.carros[i].retanguloLargura = 100;
+			this.carros[i].velocidade = 160;
+		}
+
 		this.carros[2].xPosicao = 300;
 		this.carros[2].yPosicao = ALTURA_DOS_CARROS + 30 + INTERVALO_ENTRE_CARROS + ALTURA_DOS_CARROS;
 		this.carros[2].retanguloAltura = ALTURA_DOS_CARROS;
 		this.carros[2].retanguloLargura = 150;
 		this.carros[2].velocidade = 400;
 		
-//		this.carros[3].xPosicao = 300;
-//		this.carros[3].yPosicao = ALTURA_DOS_CARROS + 30;
-//		this.carros[3].retanguloAltura = ALTURA_DOS_CARROS;
-//		this.carros[3].retanguloLargura = 100;
-//		this.carros[3].velocidade = 160;
-//		
-//		this.carros[4].xPosicao = 300;
-//		this.carros[4].yPosicao = ALTURA_DOS_CARROS + 30;
-//		this.carros[4].retanguloAltura = ALTURA_DOS_CARROS;
-//		this.carros[4].retanguloLargura = 100;
-//		this.carros[4].velocidade = 160;
-//		
-//		this.carros[5].xPosicao = 300;
-//		this.carros[5].yPosicao = ALTURA_DOS_CARROS + 30;
-//		this.carros[5].retanguloAltura = ALTURA_DOS_CARROS;
-//		this.carros[5].retanguloLargura = 100;
-//		this.carros[5].velocidade = 160;
-//		
-//		this.carros[6].xPosicao = 300;
-//		this.carros[6].yPosicao = ALTURA_DOS_CARROS + 30;
-//		this.carros[6].retanguloAltura = ALTURA_DOS_CARROS;
-//		this.carros[6].retanguloLargura = 100;
-//		this.carros[6].velocidade = 160;
-//		
-//		this.carros[7].xPosicao = 300;
-//		this.carros[7].yPosicao = ALTURA_DOS_CARROS + 30;
-//		this.carros[7].retanguloAltura = ALTURA_DOS_CARROS;
-//		this.carros[7].retanguloLargura = 100;
-//		this.carros[7].velocidade = 160;
-//		
-//		this.carros[8].xPosicao = 300;
-//		this.carros[8].yPosicao = ALTURA_DOS_CARROS + 30;
-//		this.carros[8].retanguloAltura = ALTURA_DOS_CARROS;
-//		this.carros[8].retanguloLargura = 100;
-//		this.carros[8].velocidade = 160;
+		for(int i = 3; i < 6; i++){
+			this.carros[i].xPosicao = 200 * (i-1);
+			this.carros[i].yPosicao = ALTURA_DOS_CARROS + 30 + ((INTERVALO_ENTRE_CARROS + ALTURA_DOS_CARROS) * 2) ;
+			this.carros[i].retanguloAltura = ALTURA_DOS_CARROS;
+			this.carros[i].retanguloLargura = 60;
+			this.carros[i].velocidade = 100;
+		}
+		
+		for(int i = 6; i < 9; i++){
+			this.carros[i].xPosicao = 200 * (i-1);
+			this.carros[i].yPosicao = ALTURA_DOS_CARROS + 30 + ((INTERVALO_ENTRE_CARROS + ALTURA_DOS_CARROS) * 3) ;
+			this.carros[i].retanguloAltura = ALTURA_DOS_CARROS;
+			this.carros[i].retanguloLargura = 60;
+			this.carros[i].velocidade = 134;			
+		}
+
 	}
 	
 	public void desenhar(Tela tela) {
