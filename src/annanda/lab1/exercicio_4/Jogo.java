7778fbc1b61167c71232a4cd622aea260347093b
Annanda Sousa <annanda.sousa@gmail.com>
2012-11-21 20:26:07 -0200

    Desenhado todos os carros na tela e colocado movimento em todos.

--- src/annanda/lab1/exercicio_4/Jogo.java
+++ src/annanda/lab1/exercicio_4/Jogo.java
@@ -12,7 +12,8 @@ public class Jogo {
 	public static int INTERVALO_ENTRE_CARROS = 25;
 	public static int ALTURA_DOS_CARROS = 75;
 	public static int TAMANHO_RUA = 3 * INTERVALO_ENTRE_CARROS + 4 * ALTURA_DOS_CARROS;
-	
+	public static int POSICAO_INICIAL_DA_RUA = 105;
+	public static int POSICAO_FINAL_DA_RUA = POSICAO_INICIAL_DA_RUA + TAMANHO_RUA;
 	
 	public Jogo() {	
 		this.carros = new Carro[9];
@@ -24,21 +25,21 @@ public class Jogo {
 		for(int i = 0; i < 2; i++){	
 			
 			this.carros[i].xPosicao = 200 * (i+1);
-			this.carros[i].yPosicao = ALTURA_DOS_CARROS + 30;
+			this.carros[i].yPosicao = POSICAO_INICIAL_DA_RUA;
 			this.carros[i].retanguloAltura = ALTURA_DOS_CARROS;
 			this.carros[i].retanguloLargura = 100;
 			this.carros[i].velocidade = 160;
 		}
 
 		this.carros[2].xPosicao = 300;
-		this.carros[2].yPosicao = ALTURA_DOS_CARROS + 30 + INTERVALO_ENTRE_CARROS + ALTURA_DOS_CARROS;
+		this.carros[2].yPosicao = POSICAO_INICIAL_DA_RUA + INTERVALO_ENTRE_CARROS + ALTURA_DOS_CARROS;
 		this.carros[2].retanguloAltura = ALTURA_DOS_CARROS;
 		this.carros[2].retanguloLargura = 150;
 		this.carros[2].velocidade = 400;
 		
 		for(int i = 3; i < 6; i++){
 			this.carros[i].xPosicao = 200 * (i-1);
-			this.carros[i].yPosicao = ALTURA_DOS_CARROS + 30 + ((INTERVALO_ENTRE_CARROS + ALTURA_DOS_CARROS) * 2) ;
+			this.carros[i].yPosicao = POSICAO_INICIAL_DA_RUA + ((INTERVALO_ENTRE_CARROS + ALTURA_DOS_CARROS) * 2) ;
 			this.carros[i].retanguloAltura = ALTURA_DOS_CARROS;
 			this.carros[i].retanguloLargura = 60;
 			this.carros[i].velocidade = 100;
@@ -46,7 +47,7 @@ public class Jogo {
 		
 		for(int i = 6; i < 9; i++){
 			this.carros[i].xPosicao = 200 * (i-1);
-			this.carros[i].yPosicao = ALTURA_DOS_CARROS + 30 + ((INTERVALO_ENTRE_CARROS + ALTURA_DOS_CARROS) * 3) ;
+			this.carros[i].yPosicao = POSICAO_INICIAL_DA_RUA + ((INTERVALO_ENTRE_CARROS + ALTURA_DOS_CARROS) * 3) ;
 			this.carros[i].retanguloAltura = ALTURA_DOS_CARROS;
 			this.carros[i].retanguloLargura = 60;
 			this.carros[i].velocidade = 134;			
@@ -57,7 +58,7 @@ public class Jogo {
 	public void desenhar(Tela tela) {
 		Cor corBranca = new Cor("branco");
 		tela.retangulo(0, 30, 800, 75, corBranca);
-		tela.retangulo(0, INTERVALO_ENTRE_CARROS + 75 + TAMANHO_RUA, 800, 75, corBranca);
+		tela.retangulo(0, POSICAO_FINAL_DA_RUA, 800, 75, corBranca);
 		
 		for(Carro carro : this.carros){
 			carro.desenhar(tela);
